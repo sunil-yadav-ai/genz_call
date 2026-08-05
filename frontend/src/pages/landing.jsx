e8d20f9c9b563987761b44fb5b5baa8543fbebdb
sunil yadav <bca24276117@gmail.com>
2026-08-06 01:14:25 +0530

    home page ready

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 import "../App.css";
 
 export default function LandingPage(){
@@ -14,6 +15,21 @@ export default function LandingPage(){
                     </div>
                 </div>
             </nav>
+
+            <div className='landingMainContainer'>
+                <div> <h1><span style={{ color: " #b26308" }}> Connect</span> with your loved Ones </h1>
+                <p>Cover a distance by GenZ Video Call</p>
+                <div className='linkContainer' role='button'>
+                    <Link className='link_button' to={"/home"}>Get Stated</Link>
+                </div>
+                </div>
+                
+                
+                <div>
+                    <img className='image_call' src="/photo1.png" alt="image" />
+                    <img className='image_call1' src="/photo2.png" alt="image" />
+                </div>
+            </div>
 
         </div>
     )
